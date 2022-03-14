8de482d6de4a354047b598956f76031bd8f42d8c
GYOVANNE MARTINS SABARÁ <22102360@CFP127ALUNO.LOCAL>
2022-03-14 11:11:01 -0300

    atualização

--- Imc.java
+++ Imc.java
@@ -9,9 +9,34 @@ class Imc{
 
         double imc = peso/Math.pow(altura,2);
 
-        if(imc < 17 ){
-            jop.showMessageDialog(null, "IMC = Abaixo de 17 Muito abaixo do peso");
+        if(imc > 17 ){
+            if(imc >= 18.49){
+                if(imc >= 24.99){
+                    if(imc >= 29.99){
+                        if(imc >= 34.99){
+                            if(imc >= 39.99 );
+                        }
+                        else{
+
+                        }
+                    }
+                    else{
+
+                    }
+                }
+                else{
+
+                }
+            }
+            
+
+            else{
+
+            }
         }
 
+        else{
+            jop.showMessageDialog(null, "IMC = Abaixo de 17 Muito abaixo do peso");
+        }
     }
 }
